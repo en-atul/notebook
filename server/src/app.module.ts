39a0478389d891bf84294537114135cc994b25b2
atul <atkr5496@gmail.com>
2023-01-23 21:04:43 +0530

    pub-sub module added for realtime change

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PubSubModule } from './pub-sub/pub-sub.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PubSubModule],
   controllers: [AppController],
   providers: [AppService],
 })
