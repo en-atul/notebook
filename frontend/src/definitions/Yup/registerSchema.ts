4498bafd5c03a3b210f660c00ff55912ddb76c71
atul <atkr5496@gmail.com>
2023-01-31 21:52:09 +0530

    schema fix

--- frontend/src/definitions/Yup/registerSchema.ts
+++ frontend/src/definitions/Yup/registerSchema.ts
@@ -1,38 +1,23 @@
-import * as Yup from 'yup'
+import * as Yup from "yup";
 
 export const RegisterSchema = Yup.object()
   .shape({
     fullName: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required')
-      .test('invalid', 'Must include first and last name', name =>
-        /^[a-zA-Z]+ [a-zA-Z]+/.test(name || ''),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required")
+      .test("invalid", "Must include first and last name", (name) =>
+        /^[a-zA-Z]+ [a-zA-Z]+/.test(name || "")
       ),
-    stateOfResidence: Yup.string().required('Required'),
     email: Yup.string()
       .trim()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .email('Invalid email')
-      .required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .email("Invalid email")
+      .required("Required"),
     password: Yup.string()
-      .min(8, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    phoneNumber: Yup.string()
-      .required('Required')
-      .test('invalid', 'Phone number must be valid.', phone =>
-        /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/.test(
-          phone || '',
-        ),
-      ),
-    termsOfService: Yup.boolean()
-      .required('The terms and conditions must be accepted.')
-      .test(
-        'accepted',
-        'The terms and conditions must be accepted.',
-        tos => !!tos,
-      ),
+      .min(8, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
   })
-  .required()
+  .required();
