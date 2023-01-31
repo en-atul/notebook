684c5ef41d836b7aa0e05d9064a12461175885c5
atul <atkr5496@gmail.com>
2023-01-31 20:56:44 +0530

    unnecessary ciode removed

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,20 +2,13 @@ import { Helmet } from "react-helmet";
 import { AllRoutes } from "./routes/Routes";
 import { ErrorBoundary } from "./ErrorPage";
 
-const Main = () => {
-  return <AllRoutes />;
-};
-
 const App = () => {
   return (
     <ErrorBoundary>
       <Helmet>
         <title>Notes</title>
       </Helmet>
-
-      {/* <ConnectedRouter history={history}> */}
-      <Main />
-      {/* </ConnectedRouter> */}
+      <AllRoutes />
     </ErrorBoundary>
   );
 };
