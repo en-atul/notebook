17612c35f13afd63736116c16c91a794a50a8fd8
atul <atkr5496@gmail.com>
2023-01-31 21:04:42 +0530

    query provider & devtools

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "utils";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const App = require("./app/App").default;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {process.env.NODE_ENV === "development" ? (
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      ) : null}
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
