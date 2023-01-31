73ce7c8c7944dd9e518d0bd73ab24c1891b7744a
atul <atkr5496@gmail.com>
2023-01-31 21:00:46 +0530

    react query config

---
+++ frontend/src/utils/reactQueryConfig.ts
@@ -0,0 +1,33 @@
+import { QueryClient } from "react-query";
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
+
+const cacheTime = 1000 * 60 * 60 * 24 * 5;
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const localStoragePersistor = createWebStoragePersistor({
+  storage: window.localStorage,
+});
+
+const doNotPersistQueries: string[] = [];
+
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
+  maxAge: cacheTime,
+  hydrateOptions: {},
+  dehydrateOptions: {
+    shouldDehydrateQuery: ({ queryKey }: any) => {
+      return !doNotPersistQueries.includes(queryKey);
+    },
+  },
+  buster: "notes",
+});
