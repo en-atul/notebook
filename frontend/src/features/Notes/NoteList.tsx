d5c1eeba8ee97357d4f629c5bd5af91e4bc0170f
atul <atkr5496@gmail.com>
2023-02-02 02:51:42 +0530

    create note

--- frontend/src/features/Notes/NoteList.tsx
+++ frontend/src/features/Notes/NoteList.tsx
@@ -1,32 +1,95 @@
 import classNames from "classnames";
 import { queryKeys } from "definitions";
 import { CurrentUserType, NoteType } from "interfaces";
-import { useQuery } from "react-query";
-import { queryHandler, GET_NOTES_QUERY, REFRESH_TOKEN_QUERY } from "services";
+import { useMutation, useQuery } from "react-query";
+import {
+  queryHandler,
+  CREATE_NOTE_QUERY,
+  GET_NOTES_QUERY,
+  REFRESH_TOKEN_QUERY,
+} from "services";
 import { queryClient } from "utils";
 //@ts-ignore
 import ContextMenu from "@agjs/react-right-click-menu";
 import { useRef, useState } from "react";
 
-const ActionPopup = ({ showLogout }: { showLogout: boolean }) => (
-  <div
-    style={{ margin: 0, padding: 0 }}
-    className="w-40 rounded-md border bg-white shadow-md overflow-hidden"
-  >
-    <ul className="text-sm">
-      <li className="h-10 p-3 hover:bg-gray-100">New Note</li>
+const ActionPopup = ({
+  showLogout,
+  toggleMenu,
+}: {
+  showLogout: boolean;
+  toggleMenu: () => void;
+}) => {
+  const createNoteVariables = {
+    input: {
+      title: "",
+      content: "",
+    },
+  };
 
-      {showLogout ? (
-        <li className="h-10 p-3 hover:bg-gray-100">Logout</li>
-      ) : (
-        <>
-          <li className="h-10 p-3 hover:bg-gray-100">Duplicate Note</li>
-          <li className="h-10 p-3 hover:bg-gray-100">Delete Note</li>
-        </>
-      )}
-    </ul>
-  </div>
-);
+  const { mutate } = useMutation(
+    async () => queryHandler(CREATE_NOTE_QUERY, createNoteVariables),
+    {
+      onMutate: () => {
+        toggleMenu();
+      },
+      onSuccess: (data) => {
+        if (data?.createNote) {
+          const previousNotes = queryClient.getQueryData<NoteType[]>(
+            queryKeys.notes
+          );
+          if (Array.isArray(previousNotes) && previousNotes.length)
+            queryClient.setQueryData(queryKeys.notes, () => [
+              ...previousNotes,
+              data.createNote,
+            ]);
+          else
+            queryClient.setQueryData(queryKeys.notes, () => [data.createNote]);
+
+          queryClient.setQueryData(queryKeys.selectedNote, data.createNote);
+        }
+      },
+      onError: (err: any) => {
+        const errMessage = err?.response?.errors[0]?.message;
+        console.log(errMessage);
+      },
+    }
+  );
+
+  const onLogout = () => queryClient.clear();
+
+  return (
+    <div
+      style={{ margin: 0, padding: 0 }}
+      className="w-40 rounded-md border bg-white shadow-md overflow-hidden"
+    >
+      <ul className="text-sm">
+        <li
+          className="h-10 p-3 hover:bg-gray-100"
+          role="button"
+          onClick={() => mutate()}
+        >
+          New Note
+        </li>
+
+        {showLogout ? (
+          <li
+            className="h-10 p-3 hover:bg-gray-100"
+            role="button"
+            onClick={onLogout}
+          >
+            Logout
+          </li>
+        ) : (
+          <>
+            <li className="h-10 p-3 hover:bg-gray-100">Duplicate Note</li>
+            <li className="h-10 p-3 hover:bg-gray-100">Delete Note</li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export function NoteList() {
   const [showLogout, setShowLogout] = useState(true);
@@ -72,11 +135,13 @@ export function NoteList() {
   };
 
   const toggleLogoutVisibility = () => {
-    console.log("chgcjh");
     setShowLogout(!showLogout);
   };
 
-  console.log("logg", showLogout);
+  const toggleMenu = () => {
+    setIsMenuShown(!isMenuShown);
+  };
+
   return (
     <section className="col-span-2 border-r h-full bg-white" ref={ref}>
       <div
@@ -109,9 +174,11 @@ export function NoteList() {
                   "border-b": selectedNote?.id !== note.id,
                 })}
               >
-                <p className="capitalize font-medium">{note.title}</p>
+                <p className="capitalize font-medium">
+                  {note.title || "New Note"}
+                </p>
                 <p className="capitalize text-xs font-light text-gray-500 mt-2">
-                  {note.content}
+                  {note.content || "No additional text"}
                 </p>
               </div>
             </div>
@@ -134,7 +201,9 @@ export function NoteList() {
 
       <ContextMenu
         trigger={ref}
-        component={<ActionPopup showLogout={showLogout} />}
+        component={
+          <ActionPopup toggleMenu={toggleMenu} showLogout={showLogout} />
+        }
         isOpen={isMenuShown}
         setIsOpen={(v: boolean) => {
           setIsMenuShown(v);
